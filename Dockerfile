541d01fe86deb81281fc62d029871ca94cf79512
galal-hussein <hussein.galal.ahmed.11@gmail.com>
2021-06-25 17:17:42 +0200

    add c action

---
+++ Dockerfile
@@ -0,0 +1,18 @@
+FROM debian:latest
+
+COPY entrypoint.sh /entrypoint.sh
+COPY default.cfg /default.cfg
+
+RUN apt-get update
+RUN apt-get -y install curl cmake build-essential python3 git colordiff
+RUN chmod +x entrypoint.sh
+RUN curl -L -o uncrustify-0.73.0.tar.gz https://github.com/uncrustify/uncrustify/archive/uncrustify-0.73.0.tar.gz
+RUN tar -xzf uncrustify-0.73.0.tar.gz
+WORKDIR /uncrustify-uncrustify-0.73.0
+RUN mkdir build
+WORKDIR /uncrustify-uncrustify-0.73.0/build
+RUN cmake ..
+RUN cmake --build .
+RUN cp uncrustify /usr/local/bin && chmod +x /usr/local/bin/uncrustify
+
+ENTRYPOINT ["/entrypoint.sh"]
---
+++ entrypoint.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+# Exit on any error and show execution of all commands for debugging if something goes wrong
+set -xe
+
+cd "$GITHUB_WORKSPACE"
+BRANCH_NAME=$(git rev-parse --abbrev-ref HEAD)
+DEFAULT_BRANCH=$(git symbolic-ref refs/remotes/origin/HEAD | sed 's@^refs/remotes/origin/@@')
+
+RED="\u001b[31m"
+GREEN="\u001b[32m"
+RESET="\u001b[0m"
+
+# Maintain support for uncrustify's ENV variable if it's passed in 
+# from the actions file. Otherwise the actions file could have the
+# configPath argument set
+if [[ -z $UNCRUSTIFY_CONFIG ]] && [[ -z $INPUT_CONFIGPATH ]]; then
+    CONFIG=" -c /default.cfg"
+elif [[ -z $UNCRUSTIFY_CONFIG ]] && [[ -n $INPUT_CONFIGPATH ]]; then
+    CONFIG=" -c $INPUT_CONFIGPATH"
+# If both are set, use the command line flag.
+elif [[ -n $UNCRUSTIFY_CONFIG ]] && [[ -n $INPUT_CONFIGPATH ]]; then
+    CONFIG=" -c $INPUT_CONFIGPATH"
+elif [[ -n $UNCRUSTIFY_CONFIG ]] && [[ -z $INPUT_CONFIGPATH ]]; then
+    CONFIG=""
+else
+    CONFIG=" -c /default.cfg"
+fi
+
+EXIT_VAL=0
+
+while read -r FILENAME; do
+    TMPFILE="${FILENAME}.tmp"
+    # Failure is passed to stderr so we need to redirect that to grep so we can pretty print some useful output instead of the deafult
+    # Success is passed to stdout, so we need to redirect that separately so we can capture either case.
+
+    # Allow failures here so we can capture exit status
+    set +e
+    OUT=$(uncrustify --check${CONFIG} -f ${FILENAME} 2>&1 | grep -e ^FAIL -e ^PASS | awk '{ print $2 }'; exit ${PIPESTATUS[0]})
+    RETURN_VAL=$?
+
+    # Stop allowing failures again
+    set -e 
+
+    if [[ $RETURN_VAL -gt 0 ]]; then
+        echo -e "${RED}${OUT} failed style checks.${RESET}"
+        uncrustify${CONFIG} -f ${FILENAME} -o ${TMPFILE} && colordiff -u ${FILENAME} ${TMPFILE}
+        EXIT_VAL=$RETURN_VAL
+    else
+        echo -e "${GREEN}${OUT} passed style checks.${RESET}"
+    fi
+done < <(git diff --name-status --diff-filter=AM origin/${DEFAULT_BRANCH}...${BRANCH_NAME} -- '*.c' '*.h' | awk '{ print $2 }' )
+
+exit $EXIT_VAL
